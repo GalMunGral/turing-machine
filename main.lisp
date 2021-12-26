cb32307772b33bbc32ec1f6a35f2ee897e631749
Wenqi He <hewenqi96@gmail.com>
2021-12-26 23:01:35 +0800

    tables

--- main.lisp
+++ main.lisp
@@ -32,45 +32,51 @@
 
 ;;; Find leftmost `a`
 
-(defun f (C B a)
+(defun f (C R a)
   (cond
     ((eq (G) #\E)
-      (L) (f1 C B a))
+      (L)
+      (f1 C R a))
     (T
-      (L) (f C B a))))
+      (L)
+      (f C R a))))
 
-(defun f1 (C B a) 
+(defun f1 (C R a) 
   (cond
     ((eq (G) a)
       (funcall C))
     ((any (G))
-      (R) (f1 C B a))
+      (R)
+      (f1 C R a))
     (T
-      (R) (f2 C B a))))
+      (R)
+      (f2 C R a))))
     
-(defun f2 (C B a)
+(defun f2 (C R a)
   (cond
     ((eq (G) a)
       (funcall C))
     ((any (G))
-      (R) (f1 C B a))
+      (R)
+      (f1 C R a))
     (T
-      (R) (funcall B))))
+      (R)
+      (funcall R))))
 
 ;;;; P.237
 
 ;;; Erase leftmost `a`
 
-(defun e (C B a)
-  (f ^(e1 C B a) B a))
+(defun e (C R a)
+  (f ^(e1 C R a) R a))
 
-(defun e1 (C B a)
-  (P #\Space) (funcall C))
+(defun e1 (C R a)
+  (P #\Space)
+  (funcall C))
 
 ;;; Erase all `a`
 
-(defun e* (B a)
-  (e ^(e* B a) B a))
+(defun e* (R a) (e ^(e* R a) R a))
 
 ;;; Print `b` at the end
 
@@ -80,46 +86,164 @@
 (defun pe1 (C b)
   (cond
     ((any (G))
-      (R) (R) (pe1 C b))
+      (R) (R)
+      (pe1 C b))
     (T
-      (P b) (funcall C))))
+      (P b)
+      (funcall C))))
 
 (defun l* (C)
-  (L) (funcall C))
+  (L)
+  (funcall C))
 
 (defun r* (C)
-  (R) (funcall C))
+  (R)
+  (funcall C))
 
 ;;; Find leftmost `a` then move left
 
-(defun f* (C B a)
-  (f ^(l* C) B a))
+(defun f* (C R a)
+  (f ^(l* C) R a))
 
 ;;; Find leftmost `a` then move right
 
-(defun f** (C B a)
-  (f ^(r* C) B a))
+(defun f** (C R a)
+  (f ^(r* C) R a))
 
 ;;; Write the first symbol marked with `a` to the end
 
-(defun c (C B a)
-  (f* ^(c1 C) B a))
+(defun c (C R a)
+  (f* ^(c1 C) R a))
 
 (defun c1 (C)
   (let ((b (G)))
     (if b (pe C b))))
 
+;;;; P.238
+
 ;;; Copy the first symbol marked with `a` to the end and erase the mark
 
-(defun ce (C B a)
-  (c ^(e C B a) B a))
+(defun ce (C R a)
+  (c ^(e C R a) R a))
 
 ;;; Copy all symbols marked with `a` to the end and erase the marks
 
-(defun ce* (B a)
-  (ce ^(ce* B a) B a))
+(defun ce* (R a)
+  (ce ^(ce* R a) R a))
 
-(ce* ^1 #\a)
+;;; Replace the first `a` by `b`
+
+(defun re (C R a b)
+  (f ^(re1 C R a b) R a))
+
+(defun re1 (C R a b)
+  (P #\Space) (P b)
+  (funcall C))
+
+;;; Replace all `a` by `b`
+
+(defun re* (R a b)
+  (re ^(re* R a b) R a b))
+
+(defun cr (C R a)
+  (c ^(re C R a #\a) R a))
+
+;;; Copy all symbols marked with `a` to the end
+
+(defun cr* (R a)
+  (cr ^(cr* R a) ^(re* R #\a a) a))
+
+;;; Compare the first symbol marked with `a`
+;;; with the first marked with `b`
+
+(defun cp (C R E a b)
+  (f* ^(cp1 C R b) ^(f R E b) a))
+
+(defun cp1 (C R b)
+  (let ((g (G)))
+    (f* ^(cp2 C R g) R b)))
+
+(defun cp2 (C R g)
+  (cond
+    ((eq (G) g)
+      (funcall C))
+    (t
+      (funcall R))))
+
+(defun cpe (C R E a b)
+  (cp ^(e ^(e C C b) C a) R E a b))
+
+;;; Compare sequence `a` with sequence `b`
+;;; Matched symbols at the beginning are are erased
+
+(defun cpe* (R E a b)
+  (cpe ^(cpe* R E a b) R E a b))
+
+;;;; P.239
+
+(defun q (C)
+  (cond
+    ((any (G))
+      (R)
+      (q C))
+    (t
+      (R)
+      (q1 C))))
+
+(defun q1 (C)
+  (cond
+    ((any (G))
+      (R) (q C))
+    (t ; two consecutive empty slot -> end reached
+      (funcall C)))) 
+
+;;; Find the last `a`
+
+(defun q* (C a)
+  (q ^(q1* C a)))
+
+(defun q1* (C a)
+  (cond
+    ((eq (G) a)
+      (funcall C))
+    (t
+      (L)
+      (q1* C a))))
+
+;;; Print multiple symbols at the end
+
+(defun pe2 (C a b)
+  (pe ^(pe C b) a))
+
+;;; Copy multiple sequences to the end
+
+(defun ce2* (R a b)
+  (ce* ^(ce* R b) a))
+
+(defun ce3* (R a b c)
+  (ce* ^(ce2* R b c) a))
+
+;;; Clear all marks
+
+(defun e* (C)
+  (cond
+    ((eq (G) #\E)
+      (R)
+      (e1* C))
+    (t
+      (L)
+      (e* C))))
+
+(defun e1* (C)
+  (cond
+    ((any (G))
+      (R) (P #\Space) (R)
+      (e1* C))
+    (t
+      (funcall C))))
+
+;;;; P. 244 The Universal Machine
+
 
 (close *t*)
 
